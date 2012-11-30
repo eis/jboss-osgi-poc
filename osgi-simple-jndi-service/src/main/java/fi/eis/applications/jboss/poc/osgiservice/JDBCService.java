5277d7d982487a5a2b6b5bf054a583695cfb0a1d
eis <eis@iki.fi>
2012-11-30 15:08:29 +0200

    got datasource instance

--- osgi-simple-jndi-service/src/main/java/fi/eis/applications/jboss/poc/osgiservice/JDBCService.java
+++ osgi-simple-jndi-service/src/main/java/fi/eis/applications/jboss/poc/osgiservice/JDBCService.java
@@ -1,35 +1,85 @@
 package fi.eis.applications.jboss.poc.osgiservice;
 
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import org.jboss.logging.Logger;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.jndi.JNDIContextManager;
 
 import fi.eis.applications.jboss.poc.osgiservice.api.MessageService;
 
 public class JDBCService implements MessageService {
 
-  private static Logger log = Logger.getLogger(JDBCService.class);
+	private static Logger log = Logger.getLogger(JDBCService.class);
 
-  BundleContext context;
+	private static final String JBOSS_DEFAULT_DATA_SOURCE_JNDI_NAME = "java:jboss/datasources/ExampleDS";
 
-  @Override
-  public String getMessage() {
+	BundleContext context;
 
-    context = FrameworkUtil.getBundle(JDBCService.class).getBundleContext();
-    log.info("context is " + context);
-    // obtain JNDIContextManager service from the OSGi BundleContext
-    
-    ServiceReference serviceReference = context
-        .getServiceReference("org.osgi.service.jndi.JNDIContextManager");
+	@Override
+	public String getMessage() {
 
-    log.info("reference is " + serviceReference);
+		DataSource ds = null;
+		
+		try {
+			ds = getDataSource();
+		} catch (IllegalStateException ex) {
+			log.error("error getting data source", ex);
+			return "Error getting data source: " + ex.getMessage();
+		}
 
-    // JNDIContextManager contextManager = NamingSupport.provideJNDIIntegration(
-    // context, bundle);
-    // create a context with the default environment setup
-    // Context initialContext = contextManager.newInitialContext();
-    return "JNDI lookup result: " + (serviceReference != null ? serviceReference.toString() : null);
-  }
+		log.info("lookup result is " + ds);
+
+		return "JNDI lookup result: " + ds;
+
+	}
+
+
+	private DataSource getDataSource() {
+
+		context = FrameworkUtil.getBundle(JDBCService.class).getBundleContext();
+		log.debug("context is " + context);
+
+		// obtain JNDIContextManager service from the OSGi BundleContext
+		ServiceReference serviceReference = context
+				.getServiceReference(JNDIContextManager.class.getName());
+
+		log.debug("jndi context manager reference is " + serviceReference);
+
+		JNDIContextManager contextManager = (JNDIContextManager) context
+				.getService(serviceReference);
+
+		log.debug("jndi context manager is " + contextManager);
+
+		// create a context with the default environment setup
+		Context initialContext = null;
+		try {
+			initialContext = contextManager.newInitialContext();
+		} catch (NamingException e) {
+			throw new IllegalStateException("JNDI lookup failed", e);
+		}
+
+		log.debug("jndi initial context is " + initialContext);
+
+		try {
+			return (DataSource) initialContext
+					.lookup(JBOSS_DEFAULT_DATA_SOURCE_JNDI_NAME);
+		} catch (NamingException e) {
+			throw new IllegalStateException( "DS lookup failed", e);
+		}
+
+		/*
+	     * Another alternative:
+	     * 
+	     * JNDIContextManager contextManager = NamingSupport.provideJNDIIntegration(
+	     * context, bundle);
+	     * create a context with the default environment setup
+	     * Context initialContext = contextManager.newInitialContext();
+	     */
+	}
 
 }
