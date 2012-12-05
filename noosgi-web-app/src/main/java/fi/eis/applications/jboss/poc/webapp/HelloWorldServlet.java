39fa946db68dd3ab6662e274f16b0a1e911ecaf6
eis <eis@iki.fi>
2012-12-05 19:23:38 +0200

    some more debug - show if there is a service or not

--- noosgi-web-app/src/main/java/fi/eis/applications/jboss/poc/webapp/HelloWorldServlet.java
+++ noosgi-web-app/src/main/java/fi/eis/applications/jboss/poc/webapp/HelloWorldServlet.java
@@ -2,6 +2,9 @@ package fi.eis.applications.jboss.poc.webapp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletConfig;
@@ -12,7 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.jboss.logging.Logger;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
 
@@ -64,12 +69,44 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   protected void doGet(final HttpServletRequest req,
       final HttpServletResponse resp) throws ServletException, IOException {
-    resp.setContentType("text/html");
-    PrintWriter writer = resp.getWriter();
-    writer.println(PAGE_HEADER);
-    writer.println("<h1>" + service.getMessage() + "</h1>");
-    writer.println(PAGE_FOOTER);
-    writer.close();
+		List<String> bundles = new ArrayList<String>();
+		for (Bundle bundle : context.getBundles()) {
+			bundles.add(bundle.getSymbolicName() + ":" + bundle.getVersion().toString());
+		}
+		Collections.sort(bundles);
+		List<String> services = new ArrayList<String>();
+		try {
+			for (ServiceReference ref : context.getAllServiceReferences(null, null)) {
+				services.add(context.getService(ref).toString());
+				context.ungetService(ref);
+			}
+		} catch (InvalidSyntaxException e) {
+			throw new IllegalStateException(e);
+		}
+		Collections.sort(services);
+		  
+	    resp.setContentType("text/html");
+	    PrintWriter writer = resp.getWriter();
+	    writer.println(PAGE_HEADER);
+	    writer.println("<h1>" + (service != null ? service.getMessage() : "null") + "</h1>");
+	    writer.println("Using BundleContext looked up in Servlet Context to list available OSGi Bundles:");
+		writer.println("<ul>");
+	    for (String bundleInfo : bundles) {
+	    	writer.print("<li>");
+	        writer.print(bundleInfo);
+	    	writer.println("</li>");
+	    }
+		writer.println("</ul>");
+		writer.println("<ul>");
+	    for (String service : services) {
+	    	writer.print("<li>");
+	        writer.print(service);
+	    	writer.println("</li>");
+	    }
+		writer.println("</ul>");
+	    writer.println(PAGE_FOOTER);
+	    writer.close();
+
   }
 
 }
